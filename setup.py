a04d2ea28fae9ee9a3e9a6a9dca91a1ee6c548a9
Andrew Mleczko <andrew@mleczko.net>
2012-08-08 12:56:00 +0200

    github cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.5'
 setup(name='Products.PloneboardSubscription',
       version=version,
       description="Enables email subscription to Ploneboard posts",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
